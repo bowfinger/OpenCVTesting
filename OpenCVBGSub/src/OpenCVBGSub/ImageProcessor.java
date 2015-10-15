4f0c3afc3b9b4e595ba0e013355d2c69ae50c614
bowfinger <bowfinger2k1@hotmail.com>
2015-10-15 23:24:37 +0100

    Update with Body Tracking and MOG2 - not working

--- OpenCVBGSub/src/OpenCVBGSub/ImageProcessor.java
+++ OpenCVBGSub/src/OpenCVBGSub/ImageProcessor.java
@@ -3,8 +3,13 @@ package OpenCVBGSub;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.video.BackgroundSubtractorMOG2;
 import org.opencv.video.Video;
 
@@ -14,18 +19,39 @@ public class ImageProcessor {
 	private Mat ACC_BG_IMAGE;
 	private double MOG_LEARN_RATE = 0.01;
 	private int MOG_HISTORY = 500;
-	private float MOG_THRESHOLD = 32;
+	private float MOG_THRESHOLD = 16;
 	private boolean MOG_DETECT_SHADOWS = false;
+	private CascadeClassifier BODY_CASCADE;
 	
+	public ImageProcessor(){
+		//this.INITIAL_FRAME = new Mat();
+		this.ACC_BG_IMAGE = new Mat();
+		this.BODY_CASCADE = new CascadeClassifier("C:/Users/Jamie/Downloads/opencv/sources/data/haarcascades/haarcascade_fullbody.xml");
+		
+		if(this.BODY_CASCADE.empty()){
+			System.out.println("Failed to load Cascade Classifier (BODY)");
+			return;
+		}else{
+			System.out.println("Cascade Classifier (BODY) loaded successfully.");
+		}
+	}
+	
+	public void setInitialFrame(Mat initialFrame){
+		this.INITIAL_FRAME = initialFrame;
+	}
+	
+	public void setMogThreshold(float value){
+		this.MOG_THRESHOLD = value;
+	}
+	
+	//old constructor
 	public ImageProcessor(Mat initialFrame){
-		super();
 		this.INITIAL_FRAME = initialFrame;
 		this.ACC_BG_IMAGE = new Mat();
 	}
 
-	public Mat processFrame(Mat inputFrame){
-		
-		BackgroundSubtractorMOG2 mog = Video.createBackgroundSubtractorMOG2(MOG_HISTORY, MOG_THRESHOLD, MOG_DETECT_SHADOWS);
+	public Mat processFrameNegative(Mat inputFrame){
+			
 		Mat foregroundImage = new Mat();
 		Mat foregroundThreshold = new Mat();
 		Mat grayScaleImage = new Mat();
@@ -44,24 +70,62 @@ public class ImageProcessor {
 		Mat inputFloating = new Mat();
 		grayScaleImage.convertTo(inputFloating, CvType.CV_32F);
 		Imgproc.accumulateWeighted(inputFloating, ACC_BG_IMAGE, MOG_LEARN_RATE, foregroundThreshold);
+		
 		return convertToNegative(foregroundThreshold);
+	}
+	
+	public Mat processFrameDifference(Mat inputFrame){
+		Mat foregroundImage = new Mat();
+		//basic difference subtraction
+		Core.absdiff(INITIAL_FRAME, inputFrame, foregroundImage);
+				
+		return foregroundImage;
+	}
+	
+	public Mat processFrameMOG(Mat inputFrame){
+		BackgroundSubtractorMOG2 mog = Video.createBackgroundSubtractorMOG2();
+		Mat foregroundImage = new Mat();
 		
 		//mixture of gaussians
 		//FAILS
-		//mog.apply(inputFrame, foregroundImage, MOG_LEARN_RATE);
+		//mog.apply(inputFrame, foregroundImage);
+		mog.apply(inputFrame, foregroundImage, MOG_LEARN_RATE);
 		
+		Imgproc.erode(foregroundImage, foregroundImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
+		Imgproc.dilate(foregroundImage, foregroundImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,2)));
+				
+		return foregroundImage;
+	}
+	
+	public Mat detectFullBody(Mat inputFrame){
+		Mat grayScaleImage = new Mat(inputFrame.height(), inputFrame.width(), CvType.CV_8UC1);
+		MatOfRect bodies = new MatOfRect();
 		
-		//basic difference subtraction
-		//Core.absdiff(INITIAL_FRAME, inputFrame, foregroundImage);
+		Imgproc.cvtColor(inputFrame, grayScaleImage, Imgproc.COLOR_BGRA2GRAY);
+		Imgproc.equalizeHist( grayScaleImage, grayScaleImage );
 		
-		//return foregroundImage;
+		BODY_CASCADE.detectMultiScale(grayScaleImage, bodies);
+		
+		//test
+		if (!bodies.empty()){
+			System.out.println("No of people in frame: " + bodies.toArray().length);
+		}
+		
+		for(Rect rect:bodies.toArray())  
+        {  
+             //Point center= new Point(rect.x + rect.width*0.5, rect.y + rect.height*0.5 );  
+             Imgproc.rectangle(inputFrame, rect.tl(), rect.br(), new Scalar( 255, 0, 255 ), 4, 8, 0);
+             //Imgproc.ellipse( inputFrame, center, new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 255, 0, 255 ), 4, 8, 0 );
+        }
+		return inputFrame;
 	}
 	
 	private Mat convertToNegative(Mat normalImage){
 		Mat negative = new Mat();
 		Mat white = normalImage.clone();
 		white.setTo(new Scalar(255.0));
-		Core.subtract(white, normalImage, negative);
+		Core.subtract(white, normalImage, negative);		
+		
 		return negative;
 	}
 }
--- OpenCVBGSub/src/OpenCVBGSub/OpenCVMonitor.java
+++ OpenCVBGSub/src/OpenCVBGSub/OpenCVMonitor.java
@@ -1,18 +1,29 @@
 package OpenCVBGSub;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
-public class OpenCVMonitor {
+public class OpenCVMonitor implements ChangeListener {
 	
 	private static int WINDOW_WIDTH = 680;
 	private static int WINDOW_HEIGHT = 540;
 	private static Mat INITIAL_FRAME;
 	private static Mat NORMAL_FRAME;
-	private static Mat BGSUB_FRAME;
+	private static Mat FILTERED_FRAME;
+	private static Mat DRAWN_ON_FRAME;
+	
+	private static int MOG_THRESHOLD_MIN = 0;
+	private static int MOG_THRESHOLD_MAX = 32;
+	private static int MOG_THRESHOLD_INIT = 16;
+	
+	ImageProcessor p;
 	
 	public OpenCVMonitor(){
 		//load OpenCV lib
@@ -20,7 +31,9 @@ public class OpenCVMonitor {
 		
 		INITIAL_FRAME = new Mat();
 		NORMAL_FRAME = new Mat();
-		BGSUB_FRAME = new Mat();
+		FILTERED_FRAME = new Mat();
+		DRAWN_ON_FRAME = new Mat();
+		p = new ImageProcessor();
 	}
 	
 	public void Start() throws InterruptedException{
@@ -28,24 +41,44 @@ public class OpenCVMonitor {
 								
 		//initialise windows
 		//1. main window
-		JFrame mainWindow = new JFrame("Original Image");
+		JFrame mainWindow = new JFrame("Original Image Window");
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		mainWindow.setLocation(0, 0);
 		mainWindow.setVisible(true);
 		
-		//2. bg subtract window
-		JFrame bgSubWindow = new JFrame("BG Subtract Window");
-		bgSubWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		bgSubWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		bgSubWindow.setLocation(WINDOW_WIDTH + 10, 0);
-		bgSubWindow.setVisible(true);
+		//2. filtered frame image
+		JFrame filteredWindow = new JFrame("Filtered Image Window");
+		filteredWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		filteredWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		filteredWindow.setLocation(WINDOW_WIDTH + 10, 0);
+		filteredWindow.setVisible(true);
+		
+		//3. sliders
+		JFrame sliderWindow = new JFrame("Threshold Sliders");
+		sliderWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		sliderWindow.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		sliderWindow.setLocation(0, WINDOW_HEIGHT + 10);
+		sliderWindow.setVisible(true);
+		JSlider mogThresholdSlider = new JSlider(JSlider.HORIZONTAL,MOG_THRESHOLD_MIN, MOG_THRESHOLD_MAX, MOG_THRESHOLD_INIT);
+		mogThresholdSlider.addChangeListener(this);
+
+		//Turn on labels at major tick marks.
+		mogThresholdSlider.setMajorTickSpacing(8);
+		mogThresholdSlider.setMinorTickSpacing(1);
+		mogThresholdSlider.setPaintTicks(true);
+		//framesPerSecond.setPaintLabels(true);
+		
+		JPanel sliderPanel = new JPanel();
+		sliderPanel.add(mogThresholdSlider);
+		
+		sliderWindow.setContentPane(sliderPanel);
 		
 		//initialise panel for each window
 		CustomPanel mainPanel = new CustomPanel();
 		mainWindow.setContentPane(mainPanel);
-		CustomPanel bgSubPanel = new CustomPanel();
-		bgSubWindow.setContentPane(bgSubPanel);
+		CustomPanel filteredPanel = new CustomPanel();
+		filteredWindow.setContentPane(filteredPanel);
 		
 		
 		
@@ -53,7 +86,7 @@ public class OpenCVMonitor {
 		VideoCapture camera = new VideoCapture(0);
 		
 		//used to allow time to move out of camera field of view
-		Thread.sleep(5000);
+		Thread.sleep(1000);
 		
 		//begin read and processing
 		if(camera.isOpened()){
@@ -63,21 +96,33 @@ public class OpenCVMonitor {
 			
 			if (!INITIAL_FRAME.empty()){
 				
+				//OLD
 				//load image processor
-				ImageProcessor p = new ImageProcessor(INITIAL_FRAME);
+				//ImageProcessor p = new ImageProcessor(INITIAL_FRAME);
+				p.setInitialFrame(INITIAL_FRAME);
 				
 				//infinite loop for camera feed
 				while(true){
 					camera.read(NORMAL_FRAME);
-					BGSUB_FRAME = p.processFrame(NORMAL_FRAME);
+					DRAWN_ON_FRAME = p.detectFullBody(NORMAL_FRAME);
+					
+					//bg subtraction
+					FILTERED_FRAME = p.processFrameNegative(NORMAL_FRAME);
+					//difference
+					//FILTERED_FRAME = p.processFrameDifference(NORMAL_FRAME);
+					
+					//mog - FAILS AFTER 35 FRAMES
+					//FILTERED_FRAME = p.processFrameMOG(NORMAL_FRAME);
 					
 					//buffer images
-					mainPanel.convertToBufferedImage(NORMAL_FRAME);
-					bgSubPanel.convertToBufferedImage(BGSUB_FRAME);
+					mainPanel.convertToBufferedImage(DRAWN_ON_FRAME);
+					filteredPanel.convertToBufferedImage(FILTERED_FRAME);
 					
 					//refresh panels
 					mainPanel.repaint();
-					bgSubPanel.repaint();
+					filteredPanel.repaint();
+					
+					//Thread.sleep(10);
 				}
 
 			}
@@ -86,6 +131,13 @@ public class OpenCVMonitor {
 				return;
 			}
 		}
-		
+	}
+	
+	public void stateChanged(ChangeEvent e) {
+	    JSlider source = (JSlider)e.getSource();
+	    if (!source.getValueIsAdjusting()) {
+	        int mogThreshold = (int)source.getValue();
+	        p.setMogThreshold(mogThreshold);
+	    }
 	}
 }
